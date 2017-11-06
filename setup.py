e5e0a1ba4077e254a1dd2a283299e4c3c054561a
Eric Langlois <edl@cs.toronto.edu>
2017-11-06 15:29:35 -0500

    Rearranged into src/ and tests/ directories.

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setuptools.setup(
     name='gym_utils',
     version='0.1',
     description='Open AI gym utilities.',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
     install_requires=[
         'gym',
         'numpy',
