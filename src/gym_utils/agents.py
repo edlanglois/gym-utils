d342976b938d2bc73a0d1effe65f91dcc3664083
Eric Langlois <edl@cs.toronto.edu>
2017-11-08 15:07:06 -0500

    Moved run-gym.py to gin/run-gym & allow agent-directed training

--- src/gym_utils/agents.py
+++ src/gym_utils/agents.py
@@ -10,7 +10,29 @@ class Agent():
         Args:
             env: The gym environment. An instance of `gym.Env`.
         """
-        self.env = env
+
+    @staticmethod
+    def wrap_env(env):
+        return env
+
+    def train(self, max_timesteps, env=None):
+        """Self-directed training on the environment.
+        Args:
+            max_timesteps: Maximum number of training timesteps.
+            env: Env to use for training. Uses `self.env` by default.
+
+        Returns:
+            True if training occurred.
+        """
+        return False
+
+    def save(self, directory):
+        """Save the agent state to disk."""
+        raise NotImplementedError
+
+    def load(self, directory):
+        """Load agent state from disk."""
+        raise NotImplementedError
 
     def act(self, observation):
         """Select an action for `observation`."""
